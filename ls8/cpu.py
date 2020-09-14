9ad9353a8aad485553c29e42bde49bc8164b0553
ddodds42 <57008540+ddodds42@users.noreply.github.com>
2020-09-14 14:23:08 -0400

    comparison and jump functionality

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -11,6 +11,15 @@ POP = 0b01000110
 CALL = 0b01010000
 ADD = 0b10100000
 RET = 0b00010001
+CMP = 0b10100111
+JMP = 0b01010100
+JEQ = 0b01010101
+JNE = 0b01010110
+
+# flags
+EFLAG = 0b01
+GFLAG = 0b10
+LFLAG = 0b11
 
 class CPU:
     """Main CPU class."""
@@ -42,6 +51,10 @@ class CPU:
         self.instructions[CALL] = self.fxn_call
         self.instructions[ADD] = self.fxn_add
         self.instructions[RET] = self.fxn_return
+        self.instructions[CMP] = self.fxn_compare
+        self.instructions[JMP] = self.fxn_jump
+        self.instructions[JEQ] = self.fxn_jumpeq
+        self.instructions[JNE] = self.fxn_jumpNeq
 
 
     def load(self):
@@ -71,9 +84,90 @@ class CPU:
         if op == "ADD":
             self.register[reg_a] += self.register[reg_b]
         #elif op == "SUB": etc
+        elif op == 'CMP':
+            a = self.register[reg_a]
+            b = self.register[reg_b]
+            if a > b:
+                self.fl = GFLAG
+            elif a < b:
+                self.fl = LFLAG
+            else:
+                self.fl = EFLAG
         else:
             raise Exception("Unsupported ALU operation")
+    
+    def fxn_add(self, reg, op0):
+        self.alu('ADD', reg, op0)
+        self.pc += 3
+    
+    def fxn_call(self, reg, op0):
+        rcount = self.pc + 2
+        self.sp -= 1
+        self.ram[self.sp] = rcount
+        self.pc = self.register[reg]
+    
+    def fxn_compare(self, op, op0):
+        self.alu('CMP', op, op0)
+        self.pc += 3
+    
+    def fxn_halt(self, op, op0):
+        self.running = False
+    
+    def fxn_jump(self, op, op0):
+        self.pc = self.register[op]
+    
+    def fxn_jumpeq(self, op, op0):
+        if self.fl == EFLAG:
+            self.pc = self.register[op]
+        else:
+            self.pc += 2
+    
+    def fxn_jumpNeq(self, op, op0):
+        if self.fl != EFLAG:
+            self.pc = self.register[op]
+        else:
+            self.pc += 2
+    
+    def fxn_load_integer(self, reg, int):
+        if reg < 5:
+            self.register[reg] = int
+            self.pc += 3
+        else:
+            raise Exception(
+        f'Register {reg} is reserved. Retry with a register value less than 5.'
+        )
+    
+    def fxn_multiply(self, int, multiplicand):
+        self.register[int] *= self.register[multiplicand]
+        self.pc +=3
+    
+    def fxn_print(self, reg, op0):
+        print(self.register[reg])
+        self.pc +=2
 
+    def fxn_pop(self, reg, op0):
+        self.register[reg] = self.ram[self.sp]
+        self.sp += 1
+        self.sp &= 0xff
+        self.pc += 2
+
+    def fxn_push(self, reg, op0):
+        self.sp -= 1
+        self.sp &= 0xff
+        self.ram[self.sp] = self.register[reg]
+        self.pc += 2
+    
+    def fxn_return (self, op, op0):
+        return_address = self.ram[self.sp]
+        self.pc = return_address
+        self.sp += 1
+    
+    def ram_read(self, mar):
+        return self.ram[mar]
+
+    def ram_write(self, mar, mdr):
+        self.ram[mar] = mdr
+    
     def trace(self):
         """
         Handy function to print out the CPU state. You might want to call this
@@ -93,59 +187,6 @@ class CPU:
             print(" %02X" % self.register[i], end='')
 
         print()
-    
-    def fxn_add(self, reg, op0):
-        self.alu('ADD', reg, op0)
-        self.pc += 3
-
-    def ram_read(self, mar):
-        return self.ram[mar]
-
-    def ram_write(self, mar, mdr):
-        self.ram[mar] = mdr
-    
-    def fxn_halt(self, op, op0):
-        self.running = False
-    
-    def fxn_load_integer(self, reg, int):
-        if reg < 5:
-            self.register[reg] = int
-            self.pc += 3
-        else:
-            raise Exception(
-        f'Register {reg} is reserved. Retry with a register value less than 5.'
-        )
-    
-    def fxn_print(self, reg, op0):
-        print(self.register[reg])
-        self.pc +=2
-
-    def fxn_multiply(self, int, multiplicand):
-        self.register[int] *= self.register[multiplicand]
-        self.pc +=3
-    
-    def fxn_push(self, reg, op0):
-        self.sp -= 1
-        self.sp &= 0xff
-        self.ram[self.sp] = self.register[reg]
-        self.pc += 2
-    
-    def fxn_pop(self, reg, op0):
-        self.register[reg] = self.ram[self.sp]
-        self.sp += 1
-        self.sp &= 0xff
-        self.pc += 2
-    
-    def fxn_call(self, reg, op0):
-        rcount = self.pc + 2
-        self.sp -= 1
-        self.ram[self.sp] = rcount
-        self.pc = self.register[reg]
-    
-    def fxn_return (self, op, op0):
-        return_address = self.ram[self.sp]
-        self.pc = return_address
-        self.sp += 1
 
     def run(self):
         """Run the CPU."""
